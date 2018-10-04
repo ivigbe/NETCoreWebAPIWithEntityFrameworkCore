cfa8787969d08070c5455d8bb91d028669c00bf6
ivigbe <ivanbelleil@gmail.com>
2018-10-04 15:06:47 -0300

    Agregado get y post de paises, con la base de datos en memoria paisDB.

---
+++ WebAPIPaises/Controllers/PaisController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIPaises.Models;
+
+namespace WebAPIPaises.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PaisController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Pais> Get()
+        {
+            return _context.Paises.ToList();
+        }
+
+        [HttpGet("{id}", Name = "paisCreado")]
+        public IActionResult GetById(int id)
+        {
+            var pais = this._context.Paises.FirstOrDefault(p => p.Id == id);
+
+            if (pais == null)
+                return NotFound();
+
+            return Ok(pais);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Pais pais)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            this._context.Paises.Add(pais);
+            this._context.SaveChanges();
+
+            return new CreatedAtRouteResult("paisCreado", new { id = pais.Id }, pais);
+
+            ///27:55/////
+        }
+    }
+}
--- WebAPIPaises/Startup.cs
+++ WebAPIPaises/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebAPIPaises.Models;
 
 namespace WebAPIPaises
 {
@@ -25,11 +27,12 @@ namespace WebAPIPaises
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("paisDB"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
@@ -42,6 +45,18 @@ namespace WebAPIPaises
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            if (!context.Paises.Any())
+            {
+                context.Paises.AddRange(new List<Pais>()
+                {
+                    new Pais() {Nombre = "Argentina"},
+                    new Pais() {Nombre = "EEUU"},
+                    new Pais() {Nombre = "Italia"}
+                });
+
+                context.SaveChanges();
+            }
         }
     }
 }
